13916996ce62936ebb28c158df3b5ffb79ff29a2
donfortune <donfortunet.df@gmail.com>
2023-07-15 12:22:26 +0100

    linked mysql to django project

--- django_form/models.py
+++ django_form/models.py
@@ -1,3 +1,13 @@
+
+
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'entry'
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'form',
+        'USER': 'root',
+        'PASSWORD': 'Tangban1.',
+        'HOST': 'localhost',  
+        'PORT': '3306', 
     }
 }
+
 
 
 # Password validation
